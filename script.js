c412c8fdf5f0880700662b21cf5334af04abe77f
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-11-17 20:37:38 -0800

    Add script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remainded = document.getElementById('remained');
 
+updateBigCup();
+
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx));
 });
@@ -22,4 +24,27 @@ function highlightCups(idx) {
       cup.classList.remove('full');
     }
   });
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+  // 작은 컵이 empty일 때 아무것도 보이지 않게 하기
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100} %`;
+  }
+
+  //   fullCups 일 때 remainded 안 보이게 하기
+  if (fullCups === totalCups) {
+    remainded.style.visibility = 'hidden';
+    remainded.style.height = 0;
+  } else {
+    remainded.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}`;
+  }
 }
